8ef28598e1ae8971ffc2be3656a1d20626f5cbe2
unknown <edwarz200@hotmail.com>
2018-10-08 22:04:39 -0500

    first commit

---
+++ js/mainBD.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBiYPHtleWpFu-kqJ4NjTBs_qqEYCu8WvY",
+    authDomain: "gnosis-aeb13.firebaseapp.com",
+    databaseURL: "https://gnosis-aeb13.firebaseio.com",
+    projectId: "gnosis-aeb13",
+    storageBucket: "",
+    messagingSenderId: "862940281285"
+};
+firebase.initializeApp(config);
