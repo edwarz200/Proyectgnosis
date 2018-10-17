4a1f4ea7ea037c4bd65d4e8340ee284cc19c4cee
unknown <edwarz200@hotmail.com>
2018-10-17 14:25:13 -0500

    responsive

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-    var elems = document.querySelectorAll('.carousel');
-    var optionsCarousel = ({
-      duration: 200,
-      numVisible: 3,
-    })
-    var instancescarousel = M.Carousel.init(elems, optionsCarousel);
-  
-    // instance.set(3);
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+  var elems = document.querySelectorAll('.carousel');
+  var optionsCarousel = ({
+    duration: 200,
+    numVisible: 3,
+  })
+  var instancescarousel = M.Carousel.init(elems, optionsCarousel);
 
-    var nav = document.querySelector('#nav')
-    window.addEventListener('scroll',() =>{
-      if (window.pageYOffset >= 57) {
-        nav.style.background = "#061e57e7"
-        nav.style.height = "65px"
-      } 
-      if (window.pageYOffset <= 56)  {
-        nav.style.background = ""
-        nav.style.height = ""        
-      }
-    })
-  });
-  $(".dropdown-trigger").dropdown();
+  // instance.set(3);
+
+  var nav = document.querySelector('#nav')
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset >= 57) {
+      nav.style.background = "#061e57e7"
+      nav.style.height = "65px"
+    }
+    if (window.pageYOffset <= 56) {
+      nav.style.background = ""
+      nav.style.height = ""
+    }
+  })
+});
+$(".dropdown-trigger").dropdown();
+
+setInterval(function () {
+  $('.carousel').carousel('next');
+}, 5000);
