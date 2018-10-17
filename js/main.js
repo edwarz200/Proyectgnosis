74bc38e7e6691456ddd108647e256b0408f49968
unknown <edwarz200@hotmail.com>
2018-10-17 12:39:28 -0500

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+    var elems = document.querySelectorAll('.carousel');
+    var optionsCarousel = ({
+      duration: 200,
+      numVisible: 3,
+    })
+    var instancescarousel = M.Carousel.init(elems, optionsCarousel);
+  
+    // instance.set(3);
+
+    var nav = document.querySelector('#nav')
+    window.addEventListener('scroll',() =>{
+      if (window.pageYOffset >= 57) {
+        nav.style.background = "#061e57e7"
+        nav.style.height = "65px"
+      } 
+      if (window.pageYOffset <= 56)  {
+        nav.style.background = ""
+        nav.style.height = ""        
+      }
+    })
   });
   $(".dropdown-trigger").dropdown();
